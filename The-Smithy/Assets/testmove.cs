eb62703ee9e203a3aa54e58415338278f17e2366
sky_haihai <skyhaihai2000@gmail.com>
2019-04-14 01:23:25 -0600

    basic scene

---
+++ The-Smithy/Assets/testmove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmove : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.Space)) {
+            anim.SetBool("Go", true);
+        }
+
+        Debug.Log(anim.GetBool("Go"));
+	}
+
+    public void resetGo() {
+        anim.SetBool("Go", false);
+    }
+}
