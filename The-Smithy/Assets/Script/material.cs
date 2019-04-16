3ccbe43875a4098fd241d32ff7e5307fd805eeac
yifeng <1520679052@qq.com>
2019-04-16 17:46:10 +0800

    加了素材的类和素材融合函数

---
+++ The-Smithy/Assets/Script/material.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class material
+{
+    private string name;
+    public int no;
+    private int level;
+    private int stat_addition1;
+    private int stat_addition2;
+    private int stat_addition3;
+    private int effect_addition1;
+    private int effect_addition2;
+    public material(string name, int no, int level, int stat_addition1, int stat_addition2
+        , int stat_addition3, int effect_addition1, int effect_addition2)
+    {
+        this.name = name;
+        this.no = no;
+        this.level = level;
+        this.stat_addition1 = stat_addition1;
+        this.stat_addition2 = stat_addition2;
+        this.stat_addition3 = stat_addition3;
+        this.effect_addition1 = effect_addition1;
+        this.effect_addition2 = effect_addition2;
+    }
+    public string get_name()
+    {
+        return name;
+    }
+    public int get_level()
+    {
+        return level;
+    }
+    public int get_stat_addition1()
+    {
+        return stat_addition1;
+    }
+    public int get_stat_addition2()
+    {
+        return stat_addition2;
+    }
+    public int get_stat_addition3()
+    {
+        return stat_addition3;
+    }
+    public int get_effect_addition1()
+    {
+        return effect_addition1;
+    }
+    public int get_effect_addition2()
+    {
+        return effect_addition2;
+    }
+}
