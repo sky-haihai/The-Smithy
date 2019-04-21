b04bc6fe04a01a67f4158862ba90ea105ee98832
sky_haihai <skyhaihai2000@gmail.com>
2019-04-21 01:59:39 -0600

    加了虚拟相机控制

---
+++ The-Smithy/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheSmithy.Data;
+
+public class GameController : MonoBehaviour {
+
+    PlayerData data = new PlayerData();
+
+    private void Start() {
+        if (!(SLBehavior.Load() == null)) {
+            data = SLBehavior.Load();
+            Debug.Log(data.alcohol);
+        }
+        data.alcohol = 1;
+        SLBehavior.Save(data);
+    }
+
+    private void Update() {
+        
+    }
+}
